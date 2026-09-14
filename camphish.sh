4af25ef6b55ec47b7500362c110ebe0c8065ec0d
Reworked <triphopwangwan54@gmail.com>
2026-09-14 04:02:16 -0700

    Update camphish.sh

--- camphish.sh
+++ camphish.sh
@@ -55,8 +55,7 @@ printf "\e[1;92m | |      |  ___  || |(_)| |\e[0m\e[1;77m|  _____)|  ___  |   | 
 printf "\e[1;92m | |      | (   ) || |   | |\e[0m\e[1;77m| (      | (   ) |   | |         ) || (   ) |\e[0m\n"
 printf "\e[1;92m | (____/\| )   ( || )   ( |\e[0m\e[1;77m| )      | )   ( |___) (___/\____) || )   ( |\e[0m\n"
 printf "\e[1;92m (_______/|/     \||/     \|\e[0m\e[1;77m|/       |/     \|\_______/\_______)|/     \|\e[0m\n"
-printf " \e[1;93m CamPhish Ver 2.0 \e[0m \n"
-printf " \e[1;77m www.techchip.net | youtube.com/techchipnet \e[0m \n"
+printf " \e[1;93m CamPhish Modder Ver 1.0 \e[0m \n"
 
 printf "\n"
 
